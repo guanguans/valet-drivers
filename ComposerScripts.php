0b311d20023ec0d9608845c0d3cb5b742e68cb33
yaozm <ityaozm@gmail.com>
2022-09-04 06:33:50 +0800

    Optimize scripts for Composer

--- ComposerScripts.php
+++ ComposerScripts.php
@@ -20,29 +20,32 @@ class ComposerScripts
         require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
 
         $io = $event->getIO();
-
         for (;;) {
-            $isInstalled = strtolower($io->ask("Whether to install the valet driver? [yes|no]:\n>"));
-            if ('no' === $isInstalled) {
+            $isInstalled = strtolower($io->ask("<info>Do you want to install the valet driver?</info> [yes|no]:\n>"));
+            if ('no' === $isInstalled || 'n' === $isInstalled) {
                 return;
             }
 
-            if ('yes' === $isInstalled) {
+            if ('yes' === $isInstalled || 'y' === $isInstalled) {
                 break;
             }
+
+            $io->error('Please answer yes, y, no, or n.');
         }
 
-        $io->write('Installing driver for valet...');
+        $io->write('<info>Installing driver for valet...</info>');
 
         if (! ($filesystem = new Filesystem())->isDir($driversDirectory = VALET_HOME_PATH.'/Drivers')) {
+            $io->write("<info>Creating directory($driversDirectory) for driver...</info>");
             $filesystem->mkdirAsUser($driversDirectory);
         }
 
         $files = glob(__DIR__.'/drivers/*.php');
         foreach ($files as $file) {
-            $filesystem->copyAsUser($file, $driversDirectory.'/'.pathinfo($file, PATHINFO_BASENAME));
+            $filesystem->copyAsUser($file, $to = $driversDirectory.'/'.$basename =pathinfo($file, PATHINFO_BASENAME));
+            $io->write("<warning>The `$basename` have been installed</warning>");
         }
 
-        $io->write('The driver have been installed');
+        $io->write('<info>All drivers have been installed</info>');
     }
 }
