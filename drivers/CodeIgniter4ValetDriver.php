3c246ca2c29ce40be9a707fce23ffa2a853a2070
yaozm <ityaozm@gmail.com>
2022-09-05 13:10:17 +0800

    Add driver for CodeIgniter4

---
+++ drivers/CodeIgniter4ValetDriver.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of the guanguans/valet-drivers.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+class CodeIgniter4ValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/vendor/codeigniter4/framework') && $this->asPublicPhpIndexFile($sitePath);
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if ($this->isActualFile($staticFilePath = $sitePath.'/public'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+        $_SERVER['SCRIPT_FILENAME'] = $this->asPublicPhpIndexFile($sitePath);
+        $_SERVER['SCRIPT_NAME'] = $_SERVER['PHP_SELF'] = '/index.php';
+
+        return $_SERVER['SCRIPT_FILENAME'];
+    }
+}
