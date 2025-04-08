0f9e3bd843f70c7679d1a777ee55c98d1b5d8dc5
guanguans <ityaozm@gmail.com>
2025-04-08 18:36:44 +0800

    refactor: Organize project structure and autoloading
    - Move various ValetDriver files to the 'src/Custom' directory for better organization.
    - Update Composer autoload configuration to include new namespaces.
    - Introduce a new 'helpers.php' file in 'src/Support' for shared utility functions.
    - Refactor namespace in ComposerScripts.php to align with new structure.

--- src/Support/ComposerScripts.php
+++ src/Support/ComposerScripts.php
@@ -8,7 +8,7 @@
  * This source file is subject to the MIT license that is bundled.
  */
 
-namespace Guanguans\ValetDrivers;
+namespace Guanguans\ValetDrivers\Support;
 
 use Composer\Script\Event;
 use Valet\Filesystem;
