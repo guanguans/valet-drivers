6df36d684b93ad6cc4197b2fd931a2ca9cbc0c25
guanguans <ityaozm@gmail.com>
2025-04-09 14:23:16 +0800

    test(drivers): Add CodeIgniter4ValetDriver test case
    - Introduce a new test file for the CodeIgniter4ValetDriver.
    - Validate that the driver class can be instantiated as an object.
    - Update TestCase setup to include necessary autoloading for the test environment.

---
+++ tests/Drivers/CodeIgniter4ValetDriverTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @noinspection AnonymousFunctionStaticInspection */
+/** @noinspection NullPointerExceptionInspection */
+/** @noinspection PhpPossiblePolymorphicInvocationInspection */
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection StaticClosureCanBeUsedInspection */
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/valet-drivers
+ */
+
+namespace Guanguans\ValetDriversTests\Drivers;
+
+use Valet\Drivers\Custom\CodeIgniter4ValetDriver;
+
+it('can get classes', function (): void {
+    expect(new CodeIgniter4ValetDriver)->toBeObject();
+})->group(__DIR__, __FILE__);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -53,6 +53,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
      */
     protected function setUp(): void
     {
+        require_once __DIR__.'/../vendor-bin/laravel-valet/vendor/autoload.php';
         // BypassFinals::enable(bypassReadOnly: false);
     }
 
